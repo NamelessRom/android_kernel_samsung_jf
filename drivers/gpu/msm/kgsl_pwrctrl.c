6d6fff6bce89a495a56a9badd5653abe63349dbf
Evisceration <eviscerationls@gmail.com>
2014-02-27 19:58:58 +0100

    gpu: lower overclock drastically
    Change-Id: I82f4266192b4e6d8cb18f9f49883fe60656a8cd0

--- drivers/gpu/msm/kgsl_pwrctrl.c
+++ drivers/gpu/msm/kgsl_pwrctrl.c
@@ -500,24 +500,6 @@ static int kgsl_pwrctrl_max_gpuclk_store(struct device *dev,
 		//SetMAXGPUFreq(val);
 		SetGPUpll_config(0x25, val);
 	}
-	else if (val == 545000000)
-	{
-		//pwr->pwrlevels[0].gpu_freq = val;
-		//SetMAXGPUFreq(val);
-		SetGPUpll_config(0x28, val);
-	}
-	else if (val == 600000000)
-	{
-		//pwr->pwrlevels[0].gpu_freq = val;
-		//SetMAXGPUFreq(val);
-		SetGPUpll_config(0x2C, val);
-	}
-	else if (val == 627000000)
-	{
-		//pwr->pwrlevels[0].gpu_freq = val;
-		//SetMAXGPUFreq(val);
-		SetGPUpll_config(0x2E, val);
-	}
 	//pwr->pwrlevels[0].gpu_freq = val;
 	internal_max = val;
 		
@@ -745,14 +727,13 @@ static int kgsl_pwrctrl_gpu_available_frequencies_show(
 	for (index = 0; index < pwr->num_pwrlevels - 1; index++)
 		if (index == 0)
 		{
-			num_chars += snprintf(buf + num_chars, PAGE_SIZE, "%d ",627000000);
-			num_chars += snprintf(buf + num_chars, PAGE_SIZE, "%d ",600000000);
-			num_chars += snprintf(buf + num_chars, PAGE_SIZE, "%d ",545000000);
 			num_chars += snprintf(buf + num_chars, PAGE_SIZE, "%d ",504000000);
 			num_chars += snprintf(buf + num_chars, PAGE_SIZE, "%d ",450000000);
 		}
 		else
+		{
 			num_chars += snprintf(buf + num_chars, PAGE_SIZE, "%d ",pwr->pwrlevels[index].gpu_freq);
+		}
 	buf[num_chars++] = '\n';
 	return num_chars;
 }
