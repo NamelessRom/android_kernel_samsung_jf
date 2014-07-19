e47e2385c5b234a625f540aa32bb8b3dcb73ee82
Evisceration <eviscerationls@gmail.com>
2014-07-19 18:15:08 +0200

    kgsl: disable postmortem and traces
    adreno_trace and kgsl_trace are still enabled though
    
    Change-Id: I49f75a6b40754a1dc09b95d4630fcab8f1c21fa5

--- drivers/gpu/msm2/kgsl_events.c
+++ drivers/gpu/msm2/kgsl_events.c
@@ -16,7 +16,7 @@
 #include <linux/module.h>
 #include <kgsl_device.h>
 
-#include "kgsl_trace.h"
+//#include "kgsl_trace.h"
 #include "adreno.h"
 
 static inline struct list_head *_get_list_head(struct kgsl_device *device,
@@ -49,8 +49,8 @@ static inline void _do_signal_event(struct kgsl_device *device,
 {
 	int id = event->context ? event->context->id : KGSL_MEMSTORE_GLOBAL;
 
-	trace_kgsl_fire_event(id, timestamp, type, jiffies - event->created,
-		event->func);
+	//trace_kgsl_fire_event(id, timestamp, type, jiffies - event->created,
+	//	event->func);
 
 	if (event->func)
 		event->func(device, event->priv, id, timestamp, type);
@@ -245,7 +245,7 @@ int kgsl_add_event(struct kgsl_device *device, u32 id, u32 ts,
 	 */
 
 	if (timestamp_cmp(cur_ts, ts) >= 0) {
-		trace_kgsl_fire_event(id, cur_ts, ts, 0, func);
+		//trace_kgsl_fire_event(id, cur_ts, ts, 0, func);
 
 		func(device, priv, id, ts, KGSL_EVENT_TIMESTAMP_RETIRED);
 		kgsl_context_put(context);
@@ -266,7 +266,7 @@ int kgsl_add_event(struct kgsl_device *device, u32 id, u32 ts,
 	event->owner = owner;
 	event->created = jiffies;
 
-	trace_kgsl_register_event(id, ts, func);
+	//trace_kgsl_register_event(id, ts, func);
 
 	/* Add the event to either the owning context or the global list */
 
